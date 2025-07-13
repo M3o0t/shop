04fbe563b6b912326a58ce080c56c5cc87a9ab35
Mustafa <mustafa@kali.kali.local>
2025-07-13 09:18:02 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  //i wrote this line to deploy the project , shop is the repo name
+  base :'/shop/'
 })
