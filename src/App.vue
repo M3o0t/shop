40cfdc26ccde988a15450b9ea43c8bc25a60b0a8
Mustafa <mustafa@kali.kali.local>
2025-07-14 15:05:09 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="container">
-    <nav><ul>  
-      <h2><router-link to="/" >home</router-link></h2>
-      <h2><router-link to="/contact">contact</router-link> </h2>
-      <h2><router-link to="/cart">cart</router-link></h2></ul>
+  <div>
+    <nav>  
+      <div><router-link class="route" to="/" >home</router-link></div>
+      <div><router-link class="route" to="/contact">contact</router-link> </div>
+      <div><router-link class="route" to="/cart">cart</router-link></div>
     </nav>
     <router-view></router-view>
   </div>
@@ -14,23 +14,31 @@
 
 
 
-<style scoped>
-body{
+<style >
+ body {
   margin : 0;
-   padding: 0
+   padding: 0;
+   background : linear-gradient(skyblue , rgb(223, 236, 240)) no-repeat fixed  
+
 }
 nav{
+  display : flex;
+  justify-content: space-between;
   margin: 0px 40px;
   background-color: rgb(124, 165, 156);
   border-radius: 20px;
 
-} ul {
-  display : flex;
-  justify-content: space-between;
-  margin : 0px 20px;
 }
-ul h2{
-    cursor :pointer
+nav div{
+    cursor :pointer;
+    margin : 10px;
+    font-size: 1.5rem;
 
+} 
+.route{
+text-decoration : none;
+padding: 10px 15px;
+color : #ffffff;
+font-weight : bold;
 }
 </style>
